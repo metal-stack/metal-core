db437589002bb475f891ddc9fb4a3df887f88570
Sandro Koll <sandro.koll@x-cellent.com>
2018-09-26 12:57:06 +0200

    Build binary before creating Docker image

--- build.go
+++ build.go
@@ -6,12 +6,15 @@ import (
 	"github.com/magefile/mage/sh"
 )
 
-// Creates the binary in the current directory. It will overwrite any existing binary.
+// Creates the binary metalcore in the bin subdirectory. It will overwrite any existing binary.
 func Build() error {
 	return sh.Run("go", "build", "-o", "bin/metalcore")
 }
 
-// Creates the binary in the current directory. It will overwrite any existing binary.
+// Creates the docker image metalcore that runs the binary metalcore by default.
 func Docker() error {
+	if err := Build(); err != nil {
+		return err
+	}
 	return sh.Run("docker", "build", "-t", "metalcore", ".")
 }
