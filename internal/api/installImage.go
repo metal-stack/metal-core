16da897443d5d61c026476b7ef3da2708f425add
Gerrit <tirreg91@googlemail.com>
2018-10-05 19:10:37 +0200

    Moving long poll to metal-hammer.

--- internal/api/installImage.go
+++ internal/api/installImage.go
@@ -16,27 +16,22 @@ func (c client) InstallImage(deviceId string) (int, *domain.Device) {
 	httpRequest, err := http.NewRequest(http.MethodGet, endpoint, nil)
 	httpRequest.Header.Set("Content-Type", "application/json")
 
-	var resp *http.Response
-	log.Infof("Starting long polling for device %s", deviceId)
-	for {
-		client := &http.Client{}
-		resp, err = client.Do(httpRequest)
-		if err != nil {
-			log.Debugf("Long poll request failed: %v", err)
-		}
-		if resp.StatusCode != http.StatusOK {
-			body, _ := ioutil.ReadAll(resp.Body)
-			log.Errorf("GET to wait endpoint did not succeed %v: %s", resp.Status, string(body))
-			return resp.StatusCode, nil
-		} else {
-			break
-		}
+	client := &http.Client{}
+	resp, err := client.Do(httpRequest)
+	if err != nil {
+		log.Debugf("Long poll request failed: %v", err)
 	}
+
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Errorf("unable to read response from wait call %v", err)
+		return resp.StatusCode, nil
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.Errorf("GET to wait endpoint did not succeed %v: %s", resp.Status, string(body))
 		return resp.StatusCode, nil
 	}
 
