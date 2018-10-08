68b91925dbbdd51fceed800f2c4e14c02eb084eb
Sandro Koll <sandro.koll@f-i-ts.de>
2018-10-08 13:31:58 +0200

    Log first 5 stack trace entries

--- internal/logging/logDecorator.go
+++ internal/logging/logDecorator.go
@@ -11,18 +11,27 @@ import (
 var workDir string
 
 func init() {
-	if wd, err := os.Getwd(); err == nil && len(wd) > 13 {
-		workDir = wd[0 : len(wd)-13]
+	if wd, err := os.Getwd(); err == nil {
+		workDir = wd
 	} else {
 		workDir = ""
 	}
 }
 
 func Decorate(logger *log.Entry) *log.Entry {
-	if pc, file, line, ok := runtime.Caller(1); ok {
-		funcName := runtime.FuncForPC(pc).Name()
-		file, funcName = sanitize(file, funcName)
-		return logger.WithField("stackTrace", fmt.Sprintf("%v.%v#%d", file, funcName, line))
+	st := ""
+	for i := 1; i <= 5; i++ {
+		if pc, file, line, ok := runtime.Caller(i); ok {
+			funcName := runtime.FuncForPC(pc).Name()
+			file, funcName = sanitize(file, funcName)
+			if len(st) > 0{
+				st += "\n"
+			}
+			st += fmt.Sprintf("%v::%v#%d", file, funcName, line)
+		}
+	}
+	if len(st) >  0 {
+		return logger.WithField("stackTrace", st)
 	} else {
 		return logger
 	}
@@ -30,6 +39,10 @@ func Decorate(logger *log.Entry) *log.Entry {
 
 func sanitize(file, funcName string) (string, string) {
 	if strings.Contains(file, workDir) {
+		index := strings.Index(file, "/internal")
+		if index != -1 {
+			file = file[index:]
+		}
 		file = file[len(workDir):]
 		return file, funcName[strings.LastIndex(funcName, ".")+1:]
 	} else {
