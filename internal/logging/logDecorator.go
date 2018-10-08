7db2250bc10565a06704ab93874c9d110a8b77e6
Sandro Koll <sandro.koll@f-i-ts.de>
2018-10-08 13:21:04 +0200

    Use stackTrace log field

--- internal/logging/logDecorator.go
+++ internal/logging/logDecorator.go
@@ -1,6 +1,7 @@
 package logging
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"os"
 	"runtime"
@@ -21,11 +22,7 @@ func Decorate(logger *log.Entry) *log.Entry {
 	if pc, file, line, ok := runtime.Caller(1); ok {
 		funcName := runtime.FuncForPC(pc).Name()
 		file, funcName = sanitize(file, funcName)
-		return logger.WithFields(log.Fields{
-			"file": file,
-			"line": line,
-			"func": funcName,
-		})
+		return logger.WithField("stackTrace", fmt.Sprintf("%v.%v#%d", file, funcName, line))
 	} else {
 		return logger
 	}
