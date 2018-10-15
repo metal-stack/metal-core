ebfe3e819e5528812cec755c485365595cf6fb00
Sandro Koll <sandro.koll@f-i-ts.de>
2018-10-15 17:20:05 +0200

    Improve e2e test

--- test/e2e/workflow_test.go
+++ test/e2e/workflow_test.go
@@ -1,13 +1,17 @@
 package e2e
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"git.f-i-ts.de/cloud-native/maas/metal-core/internal/domain"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 	"github.com/google/tcpproxy"
 	"github.com/magefile/mage/sh"
 	"github.com/magiconair/properties"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"os/exec"
 	"strings"
 	"testing"
@@ -15,6 +19,7 @@ import (
 )
 
 type Env struct {
+	MetalCorePort            int    `properties:"METAL_CORE_PORT"`
 	MetalAPIPort             int    `properties:"METAL_API_PORT"`
 	RethinkdbPort            int    `properties:"RETHINKDB_PORT"`
 	MetalHammerContainerName string `properties:"METAL_HAMMER_CONTAINER_NAME"`
@@ -24,74 +29,144 @@ type Env struct {
 }
 
 var (
-	env   Env
-	proxy tcpproxy.Proxy
+	env         Env
+	proxy       tcpproxy.Proxy
+	coreTraffic []string
+	apiTraffic  []string
 )
 
 func TestWorkflow(t *testing.T) {
+	if os.Getegid() != 0 {
+		runAsRoot()
+		os.Exit(0)
+	}
 	defer tearDown()
 
 	// GIVEN
-	spawnTestEnvironment(t)
+	spawnTestEnvironment()
 
 	// WHEN
-	runMetalHammer(t)
+	runMetalHammer()
 
 	// THEN
-	if out, err := sh.Output("docker", "logs", "metal-core-test"); err != nil {
+	// Verify core traffic
+	assert.Equal(t, 3, len(coreTraffic))
+
+	assert.Contains(t, coreTraffic[0], "POST /device/register", fmt.Sprintf("Metal-Cores register endpoint not called by %v container", env.MetalHammerContainerName))
+	devId := coreTraffic[0][22:strings.Index(coreTraffic[0], " HTTP")]
+	rdr := &domain.RegisterDeviceRequest{}
+	if err := json.Unmarshal([]byte(extractPayload(coreTraffic[0])), rdr); err != nil {
+		panic(err)
+	}
+	assert.Equal(t, devId, rdr.UUID)
+
+	assert.Contains(t, coreTraffic[1], "200 OK", fmt.Sprintf("Metal-APIs register endpoint not called by %v container", env.MetalCoreContainerName))
+	dev := &domain.Device{}
+	if err := json.Unmarshal([]byte(extractPayload(coreTraffic[1])), dev); err != nil {
+		panic(err)
+	}
+	assert.Equal(t, devId, dev.ID)
+
+	assert.Contains(t, coreTraffic[2], fmt.Sprintf("GET /device/install/%v", devId), fmt.Sprintf("Either Metal-APIs register endpoint threw an error or Metal-Cores install endpoint not called by %v container", env.MetalHammerContainerName))
+
+	// Verify api traffic
+	assert.Equal(t, 3, len(apiTraffic))
+
+	assert.Contains(t, apiTraffic[0], "POST /device/register", fmt.Sprintf("Metal-APIs register endpoint not called by %v container", env.MetalCoreContainerName))
+	mardr := &domain.MetalApiRegisterDeviceRequest{}
+	if err := json.Unmarshal([]byte(extractPayload(apiTraffic[0])), mardr); err != nil {
+		panic(err)
+	}
+	assert.Equal(t, devId, mardr.UUID)
+
+	assert.Contains(t, apiTraffic[1], "200 OK", fmt.Sprintf("Metal-APIs register endpoint not called by %v container", env.MetalCoreContainerName))
+	dev = &domain.Device{}
+	if err := json.Unmarshal([]byte(extractPayload(apiTraffic[1])), dev); err != nil {
+		panic(err)
+	}
+	assert.Equal(t, devId, dev.ID)
+
+	assert.Contains(t, apiTraffic[2], fmt.Sprintf("GET /device/%v/wait", devId), fmt.Sprintf("Either Metal-Cores install endpoint threw an error or Metal-APIs wait endpoint not called by %v container", env.MetalCoreContainerName))
+}
+
+func extractPayload(s string) string {
+	lines := strings.Split(s, "\n")
+	for i := 0; i < len(lines); i++ {
+		if len(strings.TrimSpace(lines[i])) == 0 {
+			return strings.Join(lines[i+1:], "")
+		}
+	}
+	return lines[len(lines)-1]
+}
+
+func runAsRoot() {
+	if mageBinary, err := sh.Output("which", "mage"); err != nil {
+		panic(err)
+	} else if wd, err := os.Getwd(); err != nil {
 		panic(err)
 	} else {
-		expected := "http://localhost:18081/device/register"
-		assert.Contains(t, out, expected, fmt.Sprintf("Metal-APIs register endpoint not called by %v container", env.MetalCoreContainerName))
-		out = forward(out, expected)
-
-		expected = "/device/install/"
-		assert.Contains(t, out, expected, fmt.Sprintf("Either Metal-APIs register endpoint threw an error or Metal-Cores install endpoint not called by %v container", env.MetalHammerContainerName))
-		out = forward(out, expected)
-
-		expected = "/wait"
-		assert.Contains(t, out, expected, fmt.Sprintf("Either Metal-Cores install endpoint threw an error or Metal-APIs wait endpoint not called by %v container", env.MetalCoreContainerName))
-		out = forward(out, expected)
+		sh.RunV("sudo", "-E", "PATH=$PATH", "bash", "-c", fmt.Sprintf("cd %v/../.. && %v test:e2e", wd, mageBinary))
 	}
 }
 
-func spawnTestEnvironment(t *testing.T) {
-	readEnvFile(t)
+func spawnTestEnvironment() {
+	readEnvFile()
 	removeMetalHammerContainer()
-	go sniffTCPPackets()
 	if _, err := sh.Output("docker-compose", "-f", "workflow_test.yaml", "up", "--force-recreate", "--remove-orphans", "-d"); err != nil {
 		panic(err)
 	}
 }
 
-func sniffTCPPackets() {
-	if handle, err := pcap.OpenLive("lo", 1800, false, pcap.BlockForever); err != nil {
-		panic(err)
-	} else if err := handle.SetBPFFilter(fmt.Sprintf("tcp and port %d", env.MetalAPIPort)); err != nil {
-		panic(err)
-	} else {
-		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-		i := 1
-		for packet := range packetSource.Packets() {
-			payload := string(packet.TransportLayer().LayerPayload())
-			if len(payload) > 0 {
-				fmt.Printf("%d:\n", i)
-				i++
-				fmt.Println(payload)
-			}
-		}
-	}
-}
-
-func runMetalHammer(t *testing.T) {
+func runMetalHammer() {
 	removeMetalHammerContainer()
 	time.Sleep(3 * time.Second)
+	waitForMetalApiContainer()
+	sniffTcpPackets()
 	if _, err := sh.Output("docker-compose", "-f", "workflow_test.yaml", "run", "-d", "--name", env.MetalHammerContainerName, "--entrypoint", "/metal-hammer", "metal-hammer"); err != nil {
 		panic(err)
 	}
 }
 
-func readEnvFile(t *testing.T) {
+func sniffTcpPackets() {
+	metalCoreHandle := createHandle(env.MetalCorePort)
+	metalApiHandle := createHandle(env.MetalAPIPort)
+	go traceTcpPackets(metalCoreHandle, &coreTraffic)
+	go traceTcpPackets(metalApiHandle, &apiTraffic)
+}
+
+func createHandle(port int) *pcap.Handle {
+	if handle, err := pcap.OpenLive("lo", 1600, false, pcap.BlockForever); err != nil {
+		panic(err)
+	} else if err := handle.SetBPFFilter(fmt.Sprintf("tcp and port %d", port)); err != nil {
+		panic(err)
+	} else {
+		return handle
+	}
+}
+
+func traceTcpPackets(handle *pcap.Handle, traffic *[]string) {
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		payload := strings.TrimSpace(string(packet.TransportLayer().LayerPayload()))
+		if len(payload) > 0 {
+			*traffic = append(*traffic, payload)
+		}
+	}
+}
+
+func waitForMetalApiContainer() {
+	for i := 0; i < 10; i++ {
+		time.Sleep(time.Second)
+		if out, err := sh.Output("docker", "logs", env.MetalAPIContainerName); err != nil {
+			panic(err)
+		} else if strings.Contains(out, "Rethinkstore connected") && strings.Contains(out, "start metal api") {
+			return
+		}
+	}
+	panic(errors.New("cannot fetch Metal-API logs"))
+}
+
+func readEnvFile() {
 	p := properties.MustLoadFile(".env", properties.UTF8)
 	if err := p.Decode(&env); err != nil {
 		panic(err)
@@ -100,14 +175,6 @@ func readEnvFile(t *testing.T) {
 
 func removeMetalHammerContainer() {
 	exec.Command("docker", "rm", "-f", env.MetalHammerContainerName).Run()
-}
-
-func forward(out string, s string) string {
-	index := strings.Index(out, s)
-	if index == -1 {
-		return ""
-	}
-	return out[index:]
 }
 
 func tearDown() {
