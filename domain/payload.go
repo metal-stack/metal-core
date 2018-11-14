40247852fb4f3f363869ea6b022e54d609c186e4
Sandro Koll <sandro.koll@f-i-ts.de>
2018-11-14 14:20:49 +0100

    Cleanup

---
+++ domain/payload.go
@@ -0,0 +1,25 @@
+package domain
+
+import (
+	"git.f-i-ts.de/cloud-native/metal/metal-core/models"
+)
+
+type (
+	BootResponse struct {
+		Kernel      string   `json:"kernel,omitempty"`
+		InitRamDisk []string `json:"initrd"`
+		CommandLine string   `json:"cmdline,omitempty"`
+	}
+
+	// Report is send back to metal-core after installation finished
+	Report struct {
+		Success bool   `json:"success,omitempty" description:"true if installation succeeded"`
+		Message string `json:"message" description:"if installation failed, the error message"`
+	}
+
+	MetalHammerRegisterDeviceRequest struct {
+		models.MetalDeviceHardware
+		UUID string            `json:"uuid,omitempty" description:"the uuid of the device to register"`
+		IPMI *models.MetalIPMI `json:"ipmi" description:"the IPMI connection configuration"`
+	}
+)
