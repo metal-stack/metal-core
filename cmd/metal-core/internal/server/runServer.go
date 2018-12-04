97e456eabe5240a8860ef7ad8eecfca3c3b315f8
Sandro Koll <sandro.koll@x-cellent.com>
2018-12-04 11:12:18 +0100

    Closes #1

--- cmd/metal-core/internal/server/runServer.go
+++ cmd/metal-core/internal/server/runServer.go
@@ -4,20 +4,12 @@ import (
 	"fmt"
 	"git.f-i-ts.de/cloud-native/metallib/zapup"
 	"github.com/emicklei/go-restful"
-	"github.com/emicklei/go-restful-openapi"
 	"go.uber.org/zap"
 	"net/http"
 )
 
 func (s server) Run() {
 	Init(s.EndpointHandler(s.AppContext))
-
-	config := restfulspec.Config{
-		WebServices:                   restful.RegisteredWebServices(),
-		APIPath:                       "/apidocs.json",
-		PostBuildSwaggerObjectHandler: enrichSwaggerObject,
-	}
-	restful.DefaultContainer.Add(restfulspec.NewOpenAPIService(config))
 
 	// enable CORS for the UI to work
 	cors := restful.CrossOriginResourceSharing{
