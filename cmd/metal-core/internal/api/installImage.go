93cd711b1622da3e8093780798d47c995b1e5922
Sandro Koll <sandro.koll@f-i-ts.de>
2019-03-25 12:33:30 +0100

    Clarify log message

--- cmd/metal-core/internal/api/installImage.go
+++ cmd/metal-core/internal/api/installImage.go
@@ -34,7 +34,7 @@ func (c *apiClient) InstallImage(machineID string) (int, *models.MetalMachineWit
 	m := ok.Payload
 
 	if m == nil || m.Machine == nil || m.Machine.Allocation == nil || m.Machine.Allocation.Image == nil {
-		zapup.MustRootLogger().Error("Got empty response from Metal-APIs wait endpoint",
+		zapup.MustRootLogger().Error("Got unexpected response from Metal-APIs wait endpoint",
 			zap.String("machineID", machineID),
 			zap.Any("machineWithToken", m),
 		)
