954363f5498aff65ed40cc50203f8559b3ec2aa3
Stefan Majer <stefan.majer@f-i-ts.de>
2019-03-25 15:01:19 +0100

    Update to latest metal-api spec, use typed error checks

--- cmd/metal-core/internal/api/installImage.go
+++ cmd/metal-core/internal/api/installImage.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"net/http"
-	"strings"
 
 	"git.f-i-ts.de/cloud-native/metal/metal-core/client/machine"
 	"git.f-i-ts.de/cloud-native/metal/metal-core/models"
@@ -16,10 +15,11 @@ func (c *apiClient) InstallImage(machineID string) (int, *models.MetalMachineWit
 
 	ok, err := c.MachineClient.WaitForAllocation(params)
 	if err != nil {
-		if strings.Contains(err.Error(), "context deadline exceeded") {
+
+		if e, ok := err.(*machine.WaitForAllocationGatewayTimeout); ok {
 			zapup.MustRootLogger().Info("Long polling timeout while GET from Metal-APIs wait endpoint",
 				zap.String("machineID", machineID),
-				zap.String("response", err.Error()),
+				zap.String("response", e.Error()),
 			)
 			return http.StatusNotModified, nil
 		}
