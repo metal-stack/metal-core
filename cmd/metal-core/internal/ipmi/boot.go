5f885dfe325165feda83795e2b2a4da0ef753e45
Sandro Koll <sandro.koll@f-i-ts.de>
2019-10-28 13:34:49 +0100

    Consider BIOS, UEFI HDD and UEFI PXE

--- cmd/metal-core/internal/ipmi/boot.go
+++ cmd/metal-core/internal/ipmi/boot.go
@@ -10,33 +10,17 @@ import (
 )
 
 const (
-	BootDevPersistentEfiQualifier = uint8(0xe0)
-	HardDiskQualifier             = uint8(0x08)
-	BiosQualifier                 = uint8(0x24)
-	PXEQualifier                  = uint8(0x04)
+	LegacyQualifier     = uint8(0xff)
+
+	PXEQualifier        = uint8(0x04)
+	DefaultHDDQualifier = uint8(0x08)
+	BIOSQualifier       = uint8(0x18)
+
+	UEFIQualifier       = uint8(0xe0)
+
+	UEFIPXEQualifier    = uint8(0x04)
+	UEFIHDDQualifier    = uint8(0x24)
 )
-
-var diskQualifiers = map[string]uint8{
-	"SYS-2029BT-HNTR":     HardDiskQualifier,
-	"SYS-2029BT-HNR":      BiosQualifier,
-	"SSG-5049P-E1CR45H":   HardDiskQualifier,
-	"MBI-6418A-T5H":       HardDiskQualifier,
-	"MBI-6219G-T7LX-PACK": HardDiskQualifier,
-	"vagrant":             HardDiskQualifier,
-}
-
-func diskQualifier(cfg *domain.IPMIConfig) uint8 {
-	if cfg.Ipmi.Fru == nil {
-		return HardDiskQualifier
-	}
-
-	diskQualifier, ok := diskQualifiers[cfg.Ipmi.Fru.ProductPartNumber]
-	if !ok {
-		return HardDiskQualifier
-	}
-
-	return diskQualifier
-}
 
 var bootMethods = map[string]func(*domain.IPMIConfig, goipmi.BootDevice) error{
 	"SYS-2029BT-HNTR":     viaIPMIRaw,
@@ -99,24 +83,27 @@ func viaIPMIRaw(cfg *domain.IPMIConfig, dev goipmi.BootDevice) error {
 	}
 
 	/*
-		Set 1st boot device to efi hard drive persistently:
-		raw 0x00 0x08 0x05 0xe0 0x08 0x00 0x00 0x00
+		Set 1st boot device to UEFI PXE persistently:
+		raw 0x00 0x08 0x05 0xe0 0x04 0x00 0x00 0x00
 
-		Set 1st boot device to efi BIOS persistently:
+		Set 1st boot device to UEFI HDD persistently:
 		raw 0x00 0x08 0x05 0xe0 0x24 0x00 0x00 0x00
 
-		Set 1st boot device to efi PXE persistently:
-		raw 0x00 0x08 0x05 0xe0 0x04 0x00 0x00 0x00
+		Set 1st boot device to BIOS persistently:
+		raw 0x00 0x08 0x05 0xff 0x18 0x00 0x00 0x00
 	*/
 
-	var bootDevQualifier uint8
+	var uefiQualifier, bootDevQualifier uint8
 	switch dev {
-	case goipmi.BootDeviceDisk:
-		bootDevQualifier = diskQualifier(cfg)
 	case goipmi.BootDeviceBios:
-		bootDevQualifier = BiosQualifier
+		uefiQualifier = LegacyQualifier
+		bootDevQualifier = BIOSQualifier
+	case goipmi.BootDeviceDisk:
+		uefiQualifier = UEFIQualifier
+		bootDevQualifier = UEFIHDDQualifier
 	case goipmi.BootDevicePxe:
-		bootDevQualifier = PXEQualifier
+		uefiQualifier = UEFIQualifier
+		bootDevQualifier = UEFIPXEQualifier
 	default:
 		return fmt.Errorf("unsupported boot device:%s", dev.String())
 	}
@@ -143,8 +130,7 @@ func viaIPMIRaw(cfg *domain.IPMIConfig, dev goipmi.BootDevice) error {
 		return err
 	}
 
-	// 0x00 0x08 0x05 0xe0 [0x08|0x024|0x04] 0x00 0x00 0x00
-	err = sendSystemBootRaw(client, goipmi.BootParamBootFlags, BootDevPersistentEfiQualifier, bootDevQualifier, 0x00, 0x00, 0x00)
+	err = sendSystemBootRaw(client, goipmi.BootParamBootFlags, uefiQualifier, bootDevQualifier, 0x00, 0x00, 0x00)
 	if err == nil {
 		if useProgress {
 			// set-in-progress = commit-write
