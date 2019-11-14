c145cc99cf1aa3954d0a22c348c001c33af702b3
Gerrit <gerrit.schwerthelm@f-i-ts.de>
2019-11-14 12:52:08 +0100

    Enhance error handling in install image function.

--- internal/api/installImage.go
+++ internal/api/installImage.go
@@ -1,8 +1,8 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
-	"strings"
 
 	"git.f-i-ts.de/cloud-native/metal/metal-core/client/machine"
 	"git.f-i-ts.de/cloud-native/metal/metal-core/models"
@@ -16,20 +16,26 @@ func (c *apiClient) InstallImage(machineID string) (int, *models.V1MachineRespon
 
 	ok, err := c.MachineClient.WaitForAllocation(params, c.Auth)
 	if err != nil {
-		// FIXME make this type safe, reuse error types from wait_responses.
-		if strings.Contains(err.Error(), "context deadline exceeded") {
+		switch e := err.(type) {
+		case *machine.WaitForAllocationGatewayTimeout:
 			zapup.MustRootLogger().Debug("Long polling timeout while GET from Metal-APIs wait endpoint",
 				zap.String("machineID", machineID),
 				zap.String("response", err.Error()),
 			)
 			return http.StatusNotModified, nil
+		case *machine.WaitForAllocationDefault:
+			zapup.MustRootLogger().Error("Failed to GET installation image from Metal-APIs wait endpoint",
+				zap.String("machineID", machineID),
+				zap.Error(fmt.Errorf(e.Error())),
+			)
+			return http.StatusInternalServerError, nil
+		default:
+			zapup.MustRootLogger().Error("Failed to GET installation image from Metal-APIs wait endpoint",
+				zap.String("machineID", machineID),
+				zap.Error(err),
+			)
+			return http.StatusInternalServerError, nil
 		}
-
-		zapup.MustRootLogger().Error("Failed to GET installation image from Metal-APIs wait endpoint",
-			zap.String("machineID", machineID),
-			zap.Error(err),
-		)
-		return http.StatusInternalServerError, nil
 	}
 
 	m := ok.Payload
