f11d516f234b091375a50821c5f36d000b6a4bd9
Sandro Koll <sandro.koll@f-i-ts.de>
2020-02-17 08:42:14 +0100

    machineid -> machineID

--- internal/api/finalizeAllocation.go
+++ internal/api/finalizeAllocation.go
@@ -7,20 +7,20 @@ import (
 	"go.uber.org/zap"
 )
 
-func (c *apiClient) FinalizeAllocation(machineid, consolePassword, primaryDisk, osPartition string) (*machine.FinalizeAllocationOK, error) {
+func (c *apiClient) FinalizeAllocation(machineID, consolePassword, primaryDisk, osPartition string) (*machine.FinalizeAllocationOK, error) {
 	body := &models.V1MachineFinalizeAllocationRequest{
 		ConsolePassword: &consolePassword,
 		Primarydisk:     &primaryDisk,
 		Ospartition:     &osPartition,
 	}
 	params := machine.NewFinalizeAllocationParams()
-	params.ID = machineid
+	params.ID = machineID
 	params.Body = body
 
 	ok, err := c.MachineClient.FinalizeAllocation(params, c.Auth)
 	if err != nil {
 		zapup.MustRootLogger().Error("Finalize failed",
-			zap.String("machineid", machineid),
+			zap.String("machineID", machineID),
 			zap.Error(err),
 		)
 	}
