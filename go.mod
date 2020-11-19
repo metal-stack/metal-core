fec5bc71d1595d9069536ffd31434e3c7dfc413c
Stefan Majer <stefan.majer@gmail.com>
2020-11-19 06:59:11 +0100

    Update go-hal and metal-go (#33)

--- go.mod
+++ go.mod
@@ -4,26 +4,26 @@ go 1.15
 
 require (
 	github.com/emicklei/go-restful-openapi/v2 v2.2.1
-	github.com/emicklei/go-restful/v3 v3.3.1
-	github.com/go-openapi/errors v0.19.7
-	github.com/go-openapi/runtime v0.19.23
-	github.com/go-openapi/spec v0.19.10
-	github.com/go-openapi/strfmt v0.19.6
-	github.com/go-openapi/swag v0.19.10
-	github.com/go-openapi/validate v0.19.11
-	github.com/google/gopacket v1.1.18
-	github.com/kelseyhightower/envconfig v1.3.0
-	github.com/metal-stack/go-hal v0.2.1
-	github.com/metal-stack/metal-go v0.10.0
-	github.com/metal-stack/metal-lib v0.6.3
-	github.com/metal-stack/metal-networker v0.3.1
+	github.com/emicklei/go-restful/v3 v3.4.0
+	github.com/go-openapi/errors v0.19.8
+	github.com/go-openapi/runtime v0.19.24
+	github.com/go-openapi/spec v0.19.14
+	github.com/go-openapi/strfmt v0.19.11
+	github.com/go-openapi/swag v0.19.12
+	github.com/go-openapi/validate v0.19.14
+	github.com/google/gopacket v1.1.19
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/metal-stack/go-hal v0.3.0
+	github.com/metal-stack/metal-go v0.11.1
+	github.com/metal-stack/metal-lib v0.6.6
+	github.com/metal-stack/metal-networker v0.5.0
 	github.com/metal-stack/security v0.4.0
 	github.com/metal-stack/v v1.0.2
 	github.com/pkg/errors v0.9.1
-	github.com/prometheus/client_golang v1.7.1
+	github.com/prometheus/client_golang v1.8.0
 	github.com/stretchr/testify v1.6.1
-	github.com/vishvananda/netlink v1.0.0
-	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
+	github.com/vishvananda/netlink v1.1.0
+	github.com/vishvananda/netns v0.0.0-20200728191858-db3c7e526aae // indirect
 	go.uber.org/zap v1.16.0
 	gopkg.in/yaml.v2 v2.3.0
 )
--- internal/ipmi/power.go
+++ internal/ipmi/power.go
@@ -2,6 +2,7 @@ package ipmi
 
 import (
 	"github.com/metal-stack/go-hal/connect"
+	halzap "github.com/metal-stack/go-hal/pkg/logger/zap"
 	"github.com/metal-stack/metal-core/pkg/domain"
 	"github.com/metal-stack/metal-lib/zapup"
 	"go.uber.org/zap"
@@ -9,7 +10,8 @@ import (
 
 // PowerOnMachine sets the power of the machine to ON
 func PowerOnMachine(cfg *domain.IPMIConfig) error {
-	outBand, err := connect.OutBand(cfg.IPMIConnection())
+	host, port, user, password := cfg.IPMIConnection()
+	outBand, err := connect.OutBand(host, port, user, password, halzap.New(zapup.MustRootLogger().Sugar()))
 	if err != nil {
 		return err
 	}
@@ -24,7 +26,8 @@ func PowerOnMachine(cfg *domain.IPMIConfig) error {
 
 // PowerOffMachine sets the power of the machine to OFF
 func PowerOffMachine(cfg *domain.IPMIConfig) error {
-	outBand, err := connect.OutBand(cfg.IPMIConnection())
+	host, port, user, password := cfg.IPMIConnection()
+	outBand, err := connect.OutBand(host, port, user, password, halzap.New(zapup.MustRootLogger().Sugar()))
 	if err != nil {
 		return err
 	}
@@ -39,7 +42,8 @@ func PowerOffMachine(cfg *domain.IPMIConfig) error {
 
 // PowerResetMachine resets the power of the machine
 func PowerResetMachine(cfg *domain.IPMIConfig) error {
-	outBand, err := connect.OutBand(cfg.IPMIConnection())
+	host, port, user, password := cfg.IPMIConnection()
+	outBand, err := connect.OutBand(host, port, user, password, halzap.New(zapup.MustRootLogger().Sugar()))
 	if err != nil {
 		return err
 	}
@@ -53,7 +57,8 @@ func PowerResetMachine(cfg *domain.IPMIConfig) error {
 
 // PowerCycleMachine cycles the power of the machine
 func PowerCycleMachine(cfg *domain.IPMIConfig) error {
-	outBand, err := connect.OutBand(cfg.IPMIConnection())
+	host, port, user, password := cfg.IPMIConnection()
+	outBand, err := connect.OutBand(host, port, user, password, halzap.New(zapup.MustRootLogger().Sugar()))
 	if err != nil {
 		return err
 	}
@@ -68,7 +73,8 @@ func PowerCycleMachine(cfg *domain.IPMIConfig) error {
 
 // PowerOnChassisIdentifyLED powers the machine chassis identify LED on indefinitely (raw 0x00 0x04 0x00 0x01)
 func PowerOnChassisIdentifyLED(cfg *domain.IPMIConfig) error {
-	outBand, err := connect.OutBand(cfg.IPMIConnection())
+	host, port, user, password := cfg.IPMIConnection()
+	outBand, err := connect.OutBand(host, port, user, password, halzap.New(zapup.MustRootLogger().Sugar()))
 	if err != nil {
 		return err
 	}
@@ -83,7 +89,8 @@ func PowerOnChassisIdentifyLED(cfg *domain.IPMIConfig) error {
 
 // PowerOffChassisIdentifyLED powers the machine chassis identify LED off (raw 0x00 0x04 0x00)
 func PowerOffChassisIdentifyLED(cfg *domain.IPMIConfig) error {
-	outBand, err := connect.OutBand(cfg.IPMIConnection())
+	host, port, user, password := cfg.IPMIConnection()
+	outBand, err := connect.OutBand(host, port, user, password, halzap.New(zapup.MustRootLogger().Sugar()))
 	if err != nil {
 		return err
 	}
