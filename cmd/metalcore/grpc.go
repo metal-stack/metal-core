fe5bebe736b0cdfa2eef2b7915cb59e7fc88aac1
Stefan Majer <stefan.majer@f-i-ts.de>
2022-05-02 11:44:46 +0200

    Add missing

---
+++ cmd/metalcore/grpc.go
@@ -0,0 +1,77 @@
+package metalcore
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"io"
+	"time"
+
+	v1 "github.com/metal-stack/metal-api/pkg/api/v1"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/keepalive"
+)
+
+type GrpcClient struct {
+	addr     string
+	dialOpts []grpc.DialOption
+	log      *zap.SugaredLogger
+}
+
+// NewGrpcClient fetches the address and certificates from metal-core needed to communicate with metal-api via grpc,
+// and returns a new grpc client that can be used to invoke all provided grpc endpoints.
+func NewGrpcClient(log *zap.SugaredLogger, address string, cert, key, caCert []byte) (*GrpcClient, error) {
+	clientCert, err := tls.X509KeyPair(cert, key)
+	if err != nil {
+		return nil, err
+	}
+
+	caCertPool := x509.NewCertPool()
+	ok := caCertPool.AppendCertsFromPEM(caCert)
+	if !ok {
+		return nil, errors.New("bad certificate")
+	}
+
+	kacp := keepalive.ClientParameters{
+		Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+		Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
+		PermitWithoutStream: true,             // send pings even without active streams
+	}
+
+	tlsConfig := &tls.Config{
+		RootCAs:      caCertPool,
+		Certificates: []tls.Certificate{clientCert},
+		MinVersion:   tls.VersionTLS12,
+	}
+	return &GrpcClient{
+		addr: address,
+		log:  log,
+		dialOpts: []grpc.DialOption{
+			grpc.WithKeepaliveParams(kacp),
+			grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)),
+			grpc.WithBlock(),
+		},
+	}, nil
+}
+
+func (c *GrpcClient) NewEventClient() (v1.EventServiceClient, io.Closer, error) {
+	conn, err := c.newConnection()
+	if err != nil {
+		return nil, nil, err
+	}
+	return v1.NewEventServiceClient(conn), conn, nil
+}
+func (c *GrpcClient) newConnection() (*grpc.ClientConn, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	conn, err := grpc.DialContext(ctx, c.addr, c.dialOpts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, err
+}
--- go.mod
+++ go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/vishvananda/netlink v1.1.0
 	go.uber.org/zap v1.21.0
 	google.golang.org/grpc v1.46.0
+	google.golang.org/protobuf v1.28.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
@@ -99,7 +100,6 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20220407144326-9054f6ed7bac // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
