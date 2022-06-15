198d30ff4aeddc63e301fa70cdae46c68221e0a1
Robert Volkmann <robert.volkmann@x-cellent.com>
2022-06-15 13:46:14 +0200

    Add SONiC/cumulus switch

--- cmd/server.go
+++ cmd/server.go
@@ -17,6 +17,8 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+const sonicVersionFile = "/etc/sonic/sonic_version.yml"
+
 func Run() {
 	cfg := &Config{}
 	if err := envconfig.Process("METAL_CORE", cfg); err != nil {
@@ -68,7 +70,12 @@ func Run() {
 		log.Fatalw("failed to create grpc client", "error", err)
 	}
 
-	nos := switcher.NewCumulus(log, cfg.FrrTplFile, cfg.InterfacesTplFile)
+	var nos core.NOS
+	if _, err := os.Stat(sonicVersionFile); err == nil {
+		nos = switcher.NewSonic(log)
+	} else {
+		nos = switcher.NewCumulus(log, cfg.FrrTplFile, cfg.InterfacesTplFile)
+	}
 
 	c := core.New(core.Config{
 		Log:                       log,
