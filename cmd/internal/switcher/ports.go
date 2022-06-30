569a565845bccfbe275b557c7bf8366a41d4a564
Robert Volkmann <robert.volkmann@x-cellent.com>
2022-06-30 09:54:15 +0200

    Add configuration of ports

---
+++ cmd/internal/switcher/ports.go
@@ -0,0 +1,72 @@
+package switcher
+
+const (
+	interfaceTable = "INTERFACE"
+	portTable      = "PORT"
+)
+
+type port struct {
+	mtu     string
+	vrfName string
+}
+
+func getPorts(cfg *Conf) map[string]*port {
+	ports := make(map[string]*port)
+
+	for _, p := range cfg.Ports.Underlay {
+		ports[p] = &port{mtu: "9216"}
+	}
+	for _, fw := range cfg.Ports.Firewalls {
+		ports[fw.Port] = &port{mtu: "9216"}
+	}
+	for vrfName, v := range cfg.Ports.Vrfs {
+		for _, p := range v.Neighbors {
+			ports[p] = &port{vrfName: vrfName, mtu: "9000"}
+		}
+	}
+	for _, p := range cfg.Ports.Unprovisioned {
+		ports[p] = &port{mtu: "9000"}
+	}
+
+	return ports
+}
+
+func applyMtus(db *ConfigDB, ports map[string]*port) error {
+	for iface, p := range ports {
+		err := db.ModEntry([]string{portTable, iface}, "mtu", p.mtu)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func applyPorts(db *ConfigDB, ports map[string]*port) error {
+	view, err := db.GetView(interfaceTable)
+	if err != nil {
+		return err
+	}
+
+	for iface, p := range ports {
+		if p.vrfName == "" {
+			continue
+		}
+
+		key := []string{interfaceTable, iface}
+		if view.Contains(key) {
+			view.Mask(key)
+			entry, err := db.GetEntry(key)
+			if err != nil {
+				return err
+			}
+			if entry["vrf_name"] == p.vrfName {
+				continue
+			}
+		}
+		err = db.SetEntry(key, "vrf_name", p.vrfName)
+		if err != nil {
+			return err
+		}
+	}
+	return view.DeleteUnmasked()
+}
---
+++ cmd/internal/switcher/ports_test.go
@@ -0,0 +1,83 @@
+package switcher
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/go-redis/redismock/v8"
+)
+
+func Test_applyMtus(t *testing.T) {
+	rdb, mock := redismock.NewClientMock()
+	db := &ConfigDB{
+		rdb:       rdb,
+		separator: "|",
+	}
+	given := map[string]*port{
+		"non-existing": {mtu: "9000"},
+		"changed":      {mtu: "new mtu"},
+		"unchanged":    {mtu: "unchanged"},
+	}
+
+	mock.ExpectHGet("PORT|non-existing", "mtu").SetErr(redis.Nil)
+	mock.ExpectHSet("PORT|non-existing", "mtu", "9000").SetVal(1)
+
+	mock.ExpectHGet("PORT|changed", "mtu").SetVal("old mtu")
+	mock.ExpectHSet("PORT|changed", "mtu", "new mtu").SetVal(1)
+
+	mock.ExpectHGet("PORT|unchanged", "mtu").SetVal("unchanged")
+
+	if err := applyMtus(db, given); err != nil {
+		t.Errorf("applyMtus() unexpected error %v", err)
+	}
+}
+
+func Test_applyPorts(t *testing.T) {
+	rdb, mock := redismock.NewClientMock()
+	db := &ConfigDB{
+		rdb:       rdb,
+		separator: "|",
+	}
+	given := map[string]*port{
+		"non-vrf":      {},
+		"non-existing": {vrfName: "vrf1"},
+		"changed":      {vrfName: "new vrf"},
+		"unchanged":    {vrfName: "unchanged"},
+	}
+
+	mock.ExpectKeys("INTERFACE|*").SetVal([]string{"INTERFACE|changed", "INTERFACE|unchanged", "leftover"})
+
+	mock.ExpectHSet("INTERFACE|non-existing", "vrf_name", "vrf1").SetVal(1)
+
+	mock.ExpectHGetAll("INTERFACE|changed").SetVal(map[string]string{"vrf_name": "old vrf"})
+	mock.ExpectHSet("INTERFACE|changed", "vrf_name", "new vrf").SetVal(1)
+
+	mock.ExpectHGetAll("INTERFACE|unchanged").SetVal(map[string]string{"vrf_name": "unchanged"})
+
+	mock.ExpectDel("leftover").SetVal(1)
+
+	if err := applyPorts(db, given); err != nil {
+		t.Errorf("applyPorts() unexpected error %v", err)
+	}
+}
+
+func Test_getPorts(t *testing.T) {
+	given := &Conf{Ports: Ports{
+		Underlay:      []string{"underlay"},
+		Unprovisioned: []string{"unprovisioned"},
+		Vrfs:          map[string]*Vrf{"vrf": {Neighbors: []string{"vrf neighbor"}}},
+		Firewalls:     map[string]*Firewall{"firewall": {Port: "firewall port"}},
+	}}
+
+	want := map[string]*port{
+		"underlay":      {mtu: "9216"},
+		"unprovisioned": {mtu: "9000"},
+		"vrf neighbor":  {mtu: "9000", vrfName: "vrf"},
+		"firewall port": {mtu: "9216"},
+	}
+
+	if got := getPorts(given); !reflect.DeepEqual(got, want) {
+		t.Errorf("getPorts() = %v, want %v", got, want)
+	}
+}
