a35722797be9ae7e68b41b856920bee17519b18d
Grigoriy Mikhalkin <grigoriymikhalkin@gmail.com>
2022-12-07 13:21:25 +0100

    upd staticd template for no-VRF leak case

--- cmd/internal/switcher/bgp_test.go
+++ cmd/internal/switcher/bgp_test.go
@@ -25,7 +25,13 @@ func TestStaticdRenderer(t *testing.T) {
 		t.Run(tt, func(t *testing.T) {
 			c := readConf(t, path.Join("test_data", tt, "conf.yaml"))
 			tpl := mustParseFS(staticdTpl)
+			
+			// Test scenario with VRF leakage
 			testTemplate(t, tpl, &c, path.Join("test_data", tt, "staticd.conf"))
+
+			// Test scenario when VRF leakage is not present
+			c.Ports.Eth0.Gateway = ""
+			testTemplate(t, tpl, &c, path.Join("test_data", tt, "staticd-no-vrf-leak.conf"))
 		})
 	}
 }
--- cmd/internal/switcher/tpl/staticd.tpl
+++ cmd/internal/switcher/tpl/staticd.tpl
@@ -6,7 +6,9 @@ enable password zebra
 log syslog {{ .LogLevel }}
 log facility local4
 !
+{{- if .Ports.Eth0.Gateway }}
 ip route 0.0.0.0/0 {{ .Ports.Eth0.Gateway }} nexthop-vrf mgmt
+{{- end }}
 !
 line vty
 !
