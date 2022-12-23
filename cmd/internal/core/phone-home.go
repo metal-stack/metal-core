554646aead650967d3644fe8a7b21a35d987c34b
Stefan Majer <stefan.majer@f-i-ts.de>
2022-12-23 10:27:25 +0100

    Enable linting and satisfy linter, dependency updates

--- cmd/internal/core/phone-home.go
+++ cmd/internal/core/phone-home.go
@@ -1,3 +1,6 @@
+//go:build client
+// +build client
+
 package core
 
 import (
@@ -84,15 +87,13 @@ func (c *Core) ConstantlyPhoneHome() {
 					msgs = append(msgs, msg)
 					return true
 				})
-				c.phoneHome(msgs)
+				c.phoneHome(ctx, msgs)
 			}
 		}
 	}()
 }
 
-func (c *Core) send(event *v1.EventServiceSendRequest) (*v1.EventServiceSendResponse, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+func (c *Core) send(ctx context.Context, event *v1.EventServiceSendRequest) (*v1.EventServiceSendResponse, error) {
 	s, err := c.eventServiceClient.Send(ctx, event)
 	if err != nil {
 		return nil, err
@@ -103,7 +104,7 @@ func (c *Core) send(event *v1.EventServiceSendRequest) (*v1.EventServiceSendResp
 	return s, err
 }
 
-func (c *Core) phoneHome(msgs []phoneHomeMessage) {
+func (c *Core) phoneHome(ctx context.Context, msgs []phoneHomeMessage) {
 	c.log.Debug("phonehome", zap.Any("machines", msgs))
 	c.log.Infow("phonehome", "machines", len(msgs))
 
@@ -119,7 +120,7 @@ func (c *Core) phoneHome(msgs []phoneHomeMessage) {
 		events[msg.machineID] = event
 	}
 
-	s, err := c.send(&v1.EventServiceSendRequest{Events: events})
+	s, err := c.send(ctx, &v1.EventServiceSendRequest{Events: events})
 	if err != nil {
 		c.log.Errorw("unable to send provisioning event back to API", "error", err)
 	}
