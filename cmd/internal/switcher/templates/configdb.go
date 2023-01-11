78cb0f271240d6a6fc2404a783e1fb1f55218880
Grigoriy Mikhalkin <grigoriymikhalkin@gmail.com>
2023-01-11 16:23:58 +0100

    split configdb updates for sonic

--- cmd/internal/switcher/templates/configdb.go
+++ cmd/internal/switcher/templates/configdb.go
@@ -14,42 +14,36 @@ import (
 )
 
 const (
-	// Tried to use "metal-core" name for the file. It doesn't work.
+	// Tried to use name that includes "-" for the file. It doesn't work.
 	// Systemd transforms "-" to "\" when %I specifier is used.
-	metalCoreConfigdb     = "/etc/sonic/metal.json"
-	metalCoreConfigdbTmp  = "/etc/sonic/metal.tmp"
-	configdbReloadService = "write-to-db"
+	metalCoreVrfConfig    = "/etc/sonic/vrfs.json"
+	metalCoreVrfConfigTmp = "/etc/sonic/vrfs.tmp"
+
+	metalCoreVlanConfig    = "/etc/sonic/vlans.json"
+	metalCoreVlanConfigTmp = "/etc/sonic/vlans.tmp"
+
+	configdbUpdateService = "write-to-db"
 
 	untagged = "untagged"
 )
 
-type configdb struct {
-	Features       map[string]*feature        `json:"FEATURE"`
-	Loopback       map[string]struct{}        `json:"LOOPBACK_INTERFACE"`
+type vrfConfig struct {
 	Ifaces         map[string]*iface          `json:"INTERFACE"`
-	Ports          map[string]*port           `json:"PORT"`
-	Vlans          map[string]*vlan2          `json:"VLAN"`
+	Vlans          map[string]*vlan           `json:"VLAN"`
 	VlanIfaces     map[string]*iface          `json:"VLAN_INTERFACE"`
-	VlanMembers    map[string]*vlanMember     `json:"VLAN_MEMBER"`
 	Vrfs           map[string]*vrf            `json:"VRF"`
-	VxlanTunnel    vxlanTunnel                `json:"VXLAN_TUNNEL"`
 	VxlanTunnelMap map[string]*vxlanTunnelMap `json:"VXLAN_TUNNEL_MAP"`
 }
 
-type feature struct {
-	State string `json:"state,omitempty"`
+type vlanConfig struct {
+	VlanMembers map[string]*vlanMember `json:"VLAN_MEMBER"`
 }
 
 type iface struct {
 	VrfName string `json:"vrf_name,omitempty"`
 }
 
-type port struct {
-	Mtu string `json:"mtu,omitempty"`
-	Fec string `json:"fec,omitempty"`
-}
-
-type vlan2 struct {
+type vlan struct {
 	VlanId      string   `json:"vlanid,omitempty"`
 	DHCPServers []string `json:"dhcp_servers,omitempty"`
 }
@@ -62,90 +56,9 @@ type vrf struct {
 	Vni string `json:"vni,omitempty"`
 }
 
-type vxlanTunnel struct {
-	Vtep vtep `json:"vtep"`
-}
-
-type vtep struct {
-	SrcIp string `json:"src_ip"`
-}
-
 type vxlanTunnelMap struct {
 	Vlan string `json:"vlan"`
 	Vni  string `json:"vni"`
-}
-
-func buildConfigdb(cfg *types.Conf, fpInfs []string) *configdb {
-	c := &configdb{
-		Features:       map[string]*feature{},
-		Loopback:       map[string]struct{}{},
-		Ifaces:         map[string]*iface{},
-		Ports:          map[string]*port{},
-		Vlans:          map[string]*vlan2{},
-		VlanIfaces:     map[string]*iface{},
-		VlanMembers:    map[string]*vlanMember{},
-		Vrfs:           map[string]*vrf{},
-		VxlanTunnel:    vxlanTunnel{vtep{SrcIp: cfg.Loopback}},
-		VxlanTunnelMap: map[string]*vxlanTunnelMap{},
-	}
-
-	c.Features["dhcp_relay"] = &feature{
-		State: "enabled",
-	}
-
-	c.Loopback["Loopback0"] = struct{}{}
-	if cfg.Loopback != "" {
-		c.Loopback[fmt.Sprintf("Loopback0|%s/32", cfg.Loopback)] = struct{}{}
-	}
-
-	for _, p := range cfg.Ports.Underlay {
-		c.Ifaces[p] = &iface{}
-		c.Ports[p] = &port{Mtu: "9216"}
-	}
-	for _, fw := range cfg.Ports.Firewalls {
-		c.Ifaces[fw.Port] = &iface{}
-		c.Ports[fw.Port] = &port{Mtu: "9216", Fec: "rs"}
-	}
-	for vrfName, v := range cfg.Ports.Vrfs {
-		for _, p := range v.Neighbors {
-			c.Ifaces[p] = &iface{vrfName}
-			c.Ports[p] = &port{Mtu: "9000", Fec: "rs"}
-		}
-		vlanId := strconv.FormatUint(uint64(v.VLANID), 10)
-		vlanName := "Vlan" + vlanId
-		vni := strconv.FormatUint(uint64(v.VNI), 10)
-		c.Vlans[vlanName] = &vlan2{VlanId: vlanId}
-		c.VlanIfaces[vlanName] = &iface{vrfName}
-		c.Vrfs[vrfName] = &vrf{vni}
-
-		tunnelMapName := fmt.Sprintf("vtep|map_%s_%s", vni, vlanName)
-		c.VxlanTunnelMap[tunnelMapName] = &vxlanTunnelMap{vlanName, vni}
-	}
-	pxeIfaceName := "Vlan4000|" + cfg.MetalCoreCIDR
-	c.Vlans["Vlan4000"] = &vlan2{VlanId: "4000", DHCPServers: cfg.DHCPServers}
-	c.VlanIfaces["Vlan4000"] = &iface{}
-	c.VlanIfaces[pxeIfaceName] = &iface{}
-	c.VxlanTunnelMap["vtep|map_104000_Vlan4000"] = &vxlanTunnelMap{"Vlan4000", "104000"}
-
-	for _, p := range cfg.Ports.Provisioned {
-		memberName := "Vlan4000|" + p
-		c.VlanMembers[memberName] = nil
-	}
-	for _, p := range cfg.Ports.Unprovisioned {
-		memberName := "Vlan4000|" + p
-		c.Ifaces[p] = &iface{}
-		c.Ports[p] = &port{Mtu: "9000", Fec: "rs"}
-		c.VlanMembers[memberName] = &vlanMember{untagged}
-	}
-
-	// remove IPs for front-panel interfaces
-	for _, inf := range fpInfs {
-		if strings.Contains(inf, "|") {
-			c.Ifaces[inf] = nil
-		}
-	}
-
-	return c
 }
 
 type ConfigdbApplier struct {
@@ -158,26 +71,128 @@ func NewConfigdbApplier(infs []string) *ConfigdbApplier {
 	}
 }
 
-func (a *ConfigdbApplier) Apply(c *types.Conf) (applied bool, err error) {
-	cfg := buildConfigdb(c, a.interfaces)
+func (a *ConfigdbApplier) Apply(cfg *types.Conf) (applied bool, err error) {
+	vrfApplied, err := a.applyVrfConfig(cfg)
+	if err != nil {
+		return false, fmt.Errorf("failed to apply VRF config: %w", err)
+	}
 
-	data, err := json.MarshalIndent(cfg, "", "  ")
+	vlanApplied, err := a.applyVrfConfig(cfg)
+	if err != nil {
+		return false, fmt.Errorf("failed to apply VLAN config: %w", err)
+	}
+
+	return vrfApplied || vlanApplied, nil
+}
+
+func buildVrfConfig(cfg *types.Conf, fpInfs []string) *vrfConfig {
+	c := &vrfConfig{
+		Vrfs:           map[string]*vrf{},
+		Ifaces:         map[string]*iface{},
+		Vlans:          map[string]*vlan{},
+		VlanIfaces:     map[string]*iface{},
+		VxlanTunnelMap: map[string]*vxlanTunnelMap{},
+	}
+
+	for vrfName, v := range cfg.Ports.Vrfs {
+		for _, p := range v.Neighbors {
+			c.Ifaces[p] = &iface{vrfName}
+		}
+		vlanId := strconv.FormatUint(uint64(v.VLANID), 10)
+		vlanName := "Vlan" + vlanId
+		vni := strconv.FormatUint(uint64(v.VNI), 10)
+		c.Vlans[vlanName] = &vlan{VlanId: vlanId}
+		c.VlanIfaces[vlanName] = &iface{vrfName}
+		c.Vrfs[vrfName] = &vrf{vni}
+
+		tunnelMapName := fmt.Sprintf("vtep|map_%s_%s", vni, vlanName)
+		c.VxlanTunnelMap[tunnelMapName] = &vxlanTunnelMap{vlanName, vni}
+	}
+
+	// Configure interfaces
+	for _, p := range cfg.Ports.Underlay {
+		c.Ifaces[p] = &iface{}
+	}
+	for _, fw := range cfg.Ports.Firewalls {
+		c.Ifaces[fw.Port] = &iface{}
+	}
+	for _, p := range cfg.Ports.Unprovisioned {
+		c.Ifaces[p] = &iface{}
+	}
+
+	// Remove IPs for front-panel interfaces
+	for _, inf := range fpInfs {
+		if strings.Contains(inf, "|") {
+			c.Ifaces[inf] = nil
+		}
+	}
+
+	return c
+}
+
+func (a *ConfigdbApplier) applyVrfConfig(cfg *types.Conf) (applied bool, err error) {
+	vrfConfig := buildVrfConfig(cfg, a.interfaces)
+
+	data, err := json.MarshalIndent(vrfConfig, "", "  ")
 	if err != nil {
 		return false, err
 	}
 
-	err = os.WriteFile(metalCoreConfigdbTmp, data, 0600)
+	err = os.WriteFile(metalCoreVrfConfigTmp, data, 0600)
 	if err != nil {
 		return false, err
 	}
 
-	applied, err = move(metalCoreConfigdbTmp, metalCoreConfigdb)
+	applied, err = move(metalCoreVrfConfigTmp, metalCoreVrfConfig)
 	if err != nil {
 		return false, err
 	}
 
 	if applied {
-		u := fmt.Sprintf("%s@%s.service", configdbReloadService, unit.UnitNamePathEscape(metalCoreConfigdb))
+		u := fmt.Sprintf("%s@%s.service", configdbUpdateService, unit.UnitNamePathEscape(metalCoreVrfConfig))
+		return applied, dbus.Start(u)
+	}
+
+	return false, nil
+}
+
+func buildVlanConfig(cfg *types.Conf) *vlanConfig {
+	c := &vlanConfig{
+		VlanMembers: map[string]*vlanMember{},
+	}
+
+	for _, p := range cfg.Ports.Provisioned {
+		memberName := "Vlan4000|" + p
+		c.VlanMembers[memberName] = nil
+	}
+	for _, p := range cfg.Ports.Unprovisioned {
+		memberName := "Vlan4000|" + p
+		c.VlanMembers[memberName] = &vlanMember{untagged}
+	}
+
+	return c
+}
+
+func (a *ConfigdbApplier) applyVlanConfig(cfg *types.Conf) (applied bool, err error) {
+	vlanConfig := buildVlanConfig(cfg)
+
+	data, err := json.MarshalIndent(vlanConfig, "", "  ")
+	if err != nil {
+		return false, err
+	}
+
+	err = os.WriteFile(metalCoreVlanConfigTmp, data, 0600)
+	if err != nil {
+		return false, err
+	}
+
+	applied, err = move(metalCoreVlanConfigTmp, metalCoreVlanConfig)
+	if err != nil {
+		return false, err
+	}
+
+	if applied {
+		u := fmt.Sprintf("%s@%s.service", configdbUpdateService, unit.UnitNamePathEscape(metalCoreVlanConfig))
 		return applied, dbus.Start(u)
 	}
 
--- cmd/internal/switcher/templates/configdb_test.go
+++ cmd/internal/switcher/templates/configdb_test.go
@@ -8,18 +8,35 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestConfigdbRenderer(t *testing.T) {
+func TestVRFApplyer(t *testing.T) {
 	tests := listTestCases()
 	for i := range tests {
 		tt := tests[i]
 		t.Run(tt, func(t *testing.T) {
 			c := readConf(t, path.Join("test_data", tt, "conf.yaml"))
-			cfg := buildConfigdb(&c, []string{"swp1", "swp1|10.0.0.10"})
+			cfg := buildVrfConfig(&c, []string{"swp1", "swp1|10.0.0.10"})
 
 			data, err := json.MarshalIndent(cfg, "", "  ")
-			require.NoError(t, err, "Couldn't marshall configdb")
+			require.NoError(t, err, "Couldn't marshall VRF config")
 			actual := string(data)
-			expected := readExpected(t, path.Join("test_data", tt, "configdb.json"))
+			expected := readExpected(t, path.Join("test_data", tt, "vrf_config.json"))
+			require.Equal(t, expected, actual, "Wanted: %s, Got: %s", expected, actual)
+		})
+	}
+}
+
+func TestVLANApplyer(t *testing.T) {
+	tests := listTestCases()
+	for i := range tests {
+		tt := tests[i]
+		t.Run(tt, func(t *testing.T) {
+			c := readConf(t, path.Join("test_data", tt, "conf.yaml"))
+			cfg := buildVlanConfig(&c)
+
+			data, err := json.MarshalIndent(cfg, "", "  ")
+			require.NoError(t, err, "Couldn't marshall VLAN config")
+			actual := string(data)
+			expected := readExpected(t, path.Join("test_data", tt, "vlan_config.json"))
 			require.Equal(t, expected, actual, "Wanted: %s, Got: %s", expected, actual)
 		})
 	}
