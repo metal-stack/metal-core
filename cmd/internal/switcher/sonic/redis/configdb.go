ac85ebf76f779e17fa2943eea3db67d0a609a124
Robert Volkmann <Robert.Volkmann@x-cellent.com>
2023-02-17 09:10:25 +0100

    Write into redis only if it necessary

--- cmd/internal/switcher/sonic/redis/configdb.go
+++ cmd/internal/switcher/sonic/redis/configdb.go
@@ -8,8 +8,13 @@ import (
 )
 
 const (
+	enable          = "enable"
 	interfaceTable  = "INTERFACE"
+	linkLocalOnly   = "ipv6_use_link_local_only"
 	vlanMemberTable = "VLAN_MEMBER"
+	taggingMode     = "tagging_mode"
+	untagged        = "untagged"
+	vrfName         = "vrf_name"
 )
 
 type configdb struct {
@@ -31,11 +36,28 @@ func newConfigdb(addr string, id int, separator string) *configdb {
 func (c *configdb) setVlanMember(ctx context.Context, interfaceName, vlan string) error {
 	key := vlanMemberTable + c.separator + vlan + c.separator + interfaceName
 
-	return c.rdb.HSet(ctx, key, "tagging_mode", "untagged").Err()
+	// If the key doesn't exist then an empty map will be returned instead of an error
+	// https://github.com/redis/go-redis/issues/1668#issuecomment-781090968
+	result, err := c.rdb.HGetAll(ctx, key).Result()
+	if err != nil {
+		return err
+	}
+	if len(result) == 1 && result[taggingMode] == untagged {
+		return nil
+	}
+
+	return c.rdb.HSet(ctx, key, taggingMode, untagged).Err()
 }
 
 func (c *configdb) deleteVlanMember(ctx context.Context, interfaceName string, vlan uint16) error {
 	key := vlanMemberTable + c.separator + "Vlan" + fmt.Sprintf("%d", vlan) + c.separator + interfaceName
+
+	err := c.rdb.Get(ctx, key).Err()
+	if err != redis.Nil {
+		return nil
+	} else if err != nil {
+		return err
+	}
 
 	return c.rdb.Del(ctx, key).Err()
 }
@@ -43,11 +65,31 @@ func (c *configdb) deleteVlanMember(ctx context.Context, interfaceName string, v
 func (c *configdb) setVrfMember(ctx context.Context, interfaceName string, vrf string) error {
 	key := interfaceTable + c.separator + interfaceName
 
-	return c.rdb.HSet(ctx, key, "ipv6_use_link_local_only", "enable", "vrf_name", vrf).Err()
+	// If the key doesn't exist then an empty map will be returned instead of an error
+	// https://github.com/redis/go-redis/issues/1668#issuecomment-781090968
+	result, err := c.rdb.HGetAll(ctx, key).Result()
+	if err != nil {
+		return err
+	}
+	if len(result) == 2 && result[linkLocalOnly] == enable && result[vrfName] == vrf {
+		return nil
+	}
+
+	return c.rdb.HSet(ctx, key, linkLocalOnly, enable, vrfName, vrf).Err()
 }
 
 func (c *configdb) deleteVrfMember(ctx context.Context, interfaceName string) error {
 	key := interfaceTable + c.separator + interfaceName
 
-	return c.rdb.HDel(ctx, key, "ipv6_use_link_local_only", "vrf_name").Err()
+	// If the key doesn't exist then an empty map will be returned instead of an error
+	// https://github.com/redis/go-redis/issues/1668#issuecomment-781090968
+	result, err := c.rdb.HGetAll(ctx, key).Result()
+	if err != nil {
+		return err
+	}
+	if _, inVrf := result[vrfName]; len(result) == 2 && result[linkLocalOnly] == enable && inVrf {
+		return c.rdb.HDel(ctx, key, "ipv6_use_link_local_only", "vrf_name").Err()
+	}
+
+	return nil
 }
