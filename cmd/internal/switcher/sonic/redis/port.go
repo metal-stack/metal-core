627326541ef34e27caef552bcc3feb41c568f772
Robert Volkmann <Robert.Volkmann@x-cellent.com>
2023-02-28 13:43:51 +0100

    only rely on config db for mtu

--- cmd/internal/switcher/sonic/redis/port.go
+++ cmd/internal/switcher/sonic/redis/port.go
@@ -3,25 +3,15 @@ package redis
 import (
 	"context"
 	"fmt"
-	"github.com/vishvananda/netlink"
 )
 
 func (a *Applier) ensurePortMTU(ctx context.Context, interfaceName string, mtu int, isFEC bool) error {
-	fromRedis, err := a.db.Config.GetPortMTU(ctx, interfaceName)
+	currentMtu, err := a.db.Config.GetPortMTU(ctx, interfaceName)
 	if err != nil {
 		return fmt.Errorf("could not retrieve port info for %s from redis: %w", interfaceName, err)
 	}
 
-	fromSys, err := netlink.LinkByName(interfaceName)
-	if err != nil {
-		return fmt.Errorf("could not retrieve port info for %s via netlink: %w", interfaceName, err)
-	}
-
-	if fromRedis != fromSys.Attrs().MTU {
-		return fmt.Errorf("different port MTU in redis %v and reported by netlink %v for interface %s", fromRedis, fromSys, interfaceName)
-	}
-
-	if fromRedis == mtu {
+	if currentMtu == mtu {
 		return nil
 	}
 
