505022f60abb09035f00d67762ec84b15aa16bdb
Robert Volkmann <Robert.Volkmann@x-cellent.com>
2023-03-08 13:07:37 +0100

    rely only on the entries in the ConfigDB

--- cmd/internal/switcher/sonic/redis/vlan.go
+++ cmd/internal/switcher/sonic/redis/vlan.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/avast/retry-go/v4"
-	"github.com/vishvananda/netlink"
 )
 
 func (a *Applier) ensureNotBridged(ctx context.Context, interfaceName string) error {
@@ -49,60 +48,17 @@ func (a *Applier) ensureNotBridged(ctx context.Context, interfaceName string) er
 }
 
 func (a *Applier) ensureInterfaceIsVlanMember(ctx context.Context, interfaceName, vlan string) error {
-	fromRedis, err := a.db.Config.GetVlanMembership(ctx, interfaceName)
+	current, err := a.db.Config.GetVlanMembership(ctx, interfaceName)
 	if err != nil {
-		return fmt.Errorf("could not retrieve vlan membership for %s from redis: %w", interfaceName, err)
+		return fmt.Errorf("could not retrieve vlan current for %s from redis: %w", interfaceName, err)
 	}
 
-	fromSys, err := getVlanMembership(interfaceName)
-	if err != nil {
-		return fmt.Errorf("could not retrieve vlan membership for %s via netlink: %w", interfaceName, err)
-	}
-
-	// an interface should belong to at most one VLAN and therefore no sorting necessary
-	if !equal(fromRedis, fromSys) {
-		return fmt.Errorf("different state in redis %v and reported by netlink %v for interface %s", fromRedis, fromSys, interfaceName)
-	}
-
-	if len(fromRedis) == 1 && fromRedis[0] == vlan {
+	if len(current) == 1 && current[0] == vlan {
 		return nil
-	} else if len(fromRedis) != 0 {
-		return fmt.Errorf("interface %s already member of a different vlan %v", interfaceName, fromRedis)
+	} else if len(current) != 0 {
+		return fmt.Errorf("interface %s already member of a different vlan %v", interfaceName, current)
 	}
 
 	a.log.Infof("add interface %s to vlan %s", interfaceName, vlan)
 	return a.db.Config.SetVlanMember(ctx, interfaceName, vlan)
-}
-
-func getVlanMembership(interfaceName string) ([]string, error) {
-	link, err := netlink.LinkByName(interfaceName)
-	if err != nil {
-		return nil, fmt.Errorf("unable to get kernel info of interface:%s %w", interfaceName, err)
-	}
-	vlansByInterface, err := netlink.BridgeVlanList()
-	if err != nil {
-		return nil, fmt.Errorf("unable to get kernel info of vlans %w", err)
-	}
-	vlanInfos, ok := vlansByInterface[int32(link.Attrs().Index)]
-	if !ok {
-		return nil, nil
-	}
-
-	vlans := make([]string, 0, len(vlanInfos))
-	for _, vlanInfo := range vlanInfos {
-		vlans = append(vlans, fmt.Sprintf("Vlan%d", vlanInfo.Vid))
-	}
-	return vlans, nil
-}
-
-func equal(a, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
 }
--- cmd/internal/switcher/sonic/redis/vrf.go
+++ cmd/internal/switcher/sonic/redis/vrf.go
@@ -3,51 +3,20 @@ package redis
 import (
 	"context"
 	"fmt"
-
-	"github.com/vishvananda/netlink"
 )
 
-func (a *Applier) ensureInterfaceIsVrfMember(ctx context.Context, interfaceName, vrf string) error {
-	fromRedis, err := a.db.Config.GetVrfMembership(ctx, interfaceName)
+func (a *Applier) ensureInterfaceIsVrfMember(ctx context.Context, interfaceName, vrfName string) error {
+	current, err := a.db.Config.GetVrfMembership(ctx, interfaceName)
 	if err != nil {
-		return fmt.Errorf("could not retrieve vrf membership for %s from redis: %w", interfaceName, err)
+		return fmt.Errorf("could not retrieve vrfName membership for %s from redis: %w", interfaceName, err)
 	}
 
-	fromSys, err := getVrfMembership(interfaceName)
-	if err != nil {
-		return fmt.Errorf("could not retrieve vrf membership for %s via netlink: %w", interfaceName, err)
-	}
-
-	if fromRedis != fromSys {
-		return fmt.Errorf("different state in redis %s and reported by netlink %v for interface %s", fromRedis, fromSys, interfaceName)
-	}
-
-	if fromRedis == vrf {
+	if current == vrfName {
 		return nil
-	} else if len(fromRedis) != 0 {
-		return fmt.Errorf("interface %s already member of a different vrf %v", interfaceName, fromRedis)
+	} else if len(current) != 0 {
+		return fmt.Errorf("interface %s already member of a different vrfName %v", interfaceName, current)
 	}
 
-	a.log.Infof("add interface %s to vrf %s", interfaceName, vrf)
-	return a.db.Config.SetVrfMember(ctx, interfaceName, vrf)
-}
-
-func getVrfMembership(interfaceName string) (string, error) {
-	link, err := netlink.LinkByName(interfaceName)
-	if err != nil {
-		return "", fmt.Errorf("unable to get kernel info of the interface %s: %w", interfaceName, err)
-	}
-
-	if link.Attrs().MasterIndex == 0 {
-		return "", nil
-	}
-
-	master, err := netlink.LinkByIndex(link.Attrs().MasterIndex)
-	if err != nil {
-		return "", fmt.Errorf("unable to get the master of the interface %s: %w", interfaceName, err)
-	}
-	if master.Type() == "vrf" {
-		return master.Attrs().Name, nil
-	}
-	return "", nil
+	a.log.Infof("add interface %s to vrfName %s", interfaceName, vrfName)
+	return a.db.Config.SetVrfMember(ctx, interfaceName, vrfName)
 }
