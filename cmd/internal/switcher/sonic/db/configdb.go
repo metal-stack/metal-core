7026a8f17b20399bb027fc9dbce48f4441c52f59
Ilja Rotar <77339620+iljarotar@users.noreply.github.com>
2025-08-20 14:07:30 +0200

    Add vrf to vni mapping (#144)

--- cmd/internal/switcher/sonic/db/configdb.go
+++ cmd/internal/switcher/sonic/db/configdb.go
@@ -154,10 +154,10 @@ func (d *ConfigDB) ExistVrf(ctx context.Context, vrf string) (bool, error) {
 	return d.c.Exists(ctx, key)
 }
 
-func (d *ConfigDB) CreateVrf(ctx context.Context, vrf string) error {
+func (d *ConfigDB) CreateVrf(ctx context.Context, vrf string, vni uint32) error {
 	key := Key{"VRF", vrf}
 
-	return d.c.HSet(ctx, key, Val{"NULL": "NULL"})
+	return d.c.HSet(ctx, key, Val{"vni": fmt.Sprintf("%d", vni)})
 }
 
 func (d *ConfigDB) DeleteVrf(ctx context.Context, vrf string) error {
